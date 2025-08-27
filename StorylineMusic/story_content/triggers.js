cf1343f7dee2277ee702d5ec5a4d8522d8dc1155
John Maddox <themaddox1116@gmail.com>
2025-08-27 15:27:41 -0400

    Add files via upload

---
+++ StorylineMusic/story_content/triggers.js
@@ -0,0 +1,48 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6jrpB23AorA":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('65pYCp1kCYE');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6SU2sOAG7im';
+const delay = 5500;
+addToTimeline(
+target.animate(
+[ {opacity: 1 }, 
+{opacity: 0 }, 
+{opacity: 1 }, 
+{opacity: 0 }, 
+{opacity: 1 } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
