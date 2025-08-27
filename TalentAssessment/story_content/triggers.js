c8c2cddf88b587c24a1836e4040237e42903771e
John Maddox <themaddox1116@gmail.com>
2025-08-27 13:18:34 -0400

    Add files via upload

---
+++ TalentAssessment/story_content/triggers.js
@@ -0,0 +1,52 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5kN64mdnp14":
+        Script1();
+        break;
+      case "5kq1ng6FZrm":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5blBJIVfUGP');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '69w4W4aTHiB';
+const pulseAmount = 0.1;
+const delay = 5000;
+addToTimeline(
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
